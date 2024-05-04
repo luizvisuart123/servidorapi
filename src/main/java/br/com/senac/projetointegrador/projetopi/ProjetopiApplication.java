e2d17aee914c9427cceda54139df2eca95fbbbbe
Luiz Carlos dos Santos Neto <c148697@corp.caixa.gov.br>
2024-05-04 08:33:18 -0300

    configurações para utilizar o banco de dados h2 em memória

--- src/main/java/br/com/senac/projetointegrador/projetopi/ProjetopiApplication.java
+++ src/main/java/br/com/senac/projetointegrador/projetopi/ProjetopiApplication.java
@@ -1,7 +1,9 @@
 package br.com.senac.projetointegrador.projetopi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjetopiApplication {
@@ -9,5 +11,9 @@ public class ProjetopiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetopiApplication.class, args);
 	}
-
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
--- src/main/java/br/com/senac/projetointegrador/projetopi/model/User.java
+++ src/main/java/br/com/senac/projetointegrador/projetopi/model/User.java
@@ -1,5 +1,18 @@
 package br.com.senac.projetointegrador.projetopi.model;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+
+@EntityScan
 public class User {
+	
+	@Id
+	private Integer id;
+	
+	private String user;
+	
+	private String password;
+	
+	
 
 }
