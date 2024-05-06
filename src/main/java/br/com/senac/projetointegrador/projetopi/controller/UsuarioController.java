0986e75465f56fda91351cc77a18cf52da61721e
Luiz Carlos dos Santos Neto <c148697@corp.caixa.gov.br>
2024-05-06 14:57:36 -0300

    adicionar cliente implementado

--- src/main/java/br/com/senac/projetointegrador/projetopi/controller/UsuarioController.java
+++ src/main/java/br/com/senac/projetointegrador/projetopi/controller/UsuarioController.java
@@ -3,7 +3,11 @@ package br.com.senac.projetointegrador.projetopi.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +23,10 @@ public class UsuarioController {
 	
 	
 	//criar
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> inserir (@RequestBody UsuarioDTO dto) {
+        return new ResponseEntity<>(this.service.salvar(dto), HttpStatus.CREATED);
+    }
 	
 	
 	//ler
